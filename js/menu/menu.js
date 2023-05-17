dac9522a547efed1065c1a8f6cf408a93d1b58f4
GeoJina <jinaseo101@gmail.com>
2023-05-17 12:08:48 +0900

    jn-체크박스 활성화, 비활성화 코드  추가

--- js/menu/menu.js
+++ js/menu/menu.js
@@ -54,3 +54,52 @@ $('.selector_tab').click(function(){
         $('.selector_tab a').css('background-image', 'url(../../../img/menu/arrowD.png)');
     }
 });
+
+// HTML 요소 선택
+const breadAllCheckbox = document.querySelector('#bread_all');
+const otherCheckboxes = document.querySelectorAll('input[type="checkbox"]:not(#bread_all)');
+
+// '모든 빵 보기' 체크박스 초기 상태 설정
+breadAllCheckbox.checked = true;
+
+// 이벤트 핸들러 등록
+breadAllCheckbox.addEventListener('change', handleBreadAllCheckboxChange);
+otherCheckboxes.forEach(checkbox => checkbox.addEventListener('change', handleOtherCheckboxesChange));
+
+// '모든 빵 보기' 체크박스 변경 시 이벤트 처리
+function handleBreadAllCheckboxChange() {
+  if (breadAllCheckbox.checked) {
+    otherCheckboxes.forEach(checkbox => {
+      checkbox.checked = false;
+    });
+  } else {
+    otherCheckboxes.forEach(checkbox => {
+      checkbox.disabled = false;
+    });
+  }
+}
+
+// 다른 체크박스 변경 시 이벤트 처리
+function handleOtherCheckboxesChange() {
+  if (breadAllCheckbox.checked) {
+    breadAllCheckbox.checked = false;
+  }
+  checkIfAllOtherCheckboxesUnchecked();
+  checkIfAnyOtherCheckboxChecked();
+}
+
+// 모든 다른 체크박스가 선택 해제되었는지 확인
+function checkIfAllOtherCheckboxesUnchecked() {
+  const uncheckedCheckboxes = Array.from(otherCheckboxes).filter(checkbox => !checkbox.checked);
+  if (uncheckedCheckboxes.length === otherCheckboxes.length - 1) {
+    breadAllCheckbox.checked = true;
+  }
+}
+
+// 다른 체크박스 중 하나라도 선택되었는지 확인
+function checkIfAnyOtherCheckboxChecked() {
+  const checkedCheckboxes = Array.from(otherCheckboxes).filter(checkbox => checkbox.checked);
+  if (checkedCheckboxes.length > 0) {
+    breadAllCheckbox.checked = false;
+  }
+}
