07af97f3c665c97c956370b0a5a9aeb65a9c368f
hellicopthecat <114468016+hellicopthecat@users.noreply.github.com>
2023-05-21 02:05:02 +0900

    jh_edit html/css/js

--- js/eventpage/eventPage.js
+++ js/eventpage/eventPage.js
@@ -1,47 +1,10 @@
-const eventWin = document.getElementById("event_winner");
-const winnerList = eventWin.querySelectorAll("li");
+const eventPageList = document.querySelectorAll(".eventpage__list ul li a");
+const dialogs = document.querySelectorAll(".eventpage__modal dialog");
 
-winnerList.forEach((each) => {
-  function handleClick(event) {
-    event.preventDefault();
-    const img = each.querySelectorAll("img");
+eventPageList.forEach((eventList, index) => {
+  const handleEventPageModal = () => {
+    dialogs[index].showModal();
+  };
 
-    img.forEach((att) => {
-      // console.dir(att);
-      if (att.classList.contains("slideup_active")) {
-        att.classList.remove("slideup_active");
-        att.style.height = "auto";
-
-        const imgHeight = `${att.clientHeight}px`;
-
-        att.style.height = "0px";
-        setTimeout(() => {
-          att.style.height = imgHeight;
-          // att.style.height = "500px";
-        }, 0);
-      } else {
-        att.style.height = "0px";
-        att.addEventListener(
-          "transitionend",
-          () => {
-            att.classList.add("slideup_active");
-          },
-          {once: true}
-        );
-      }
-      winnerList.forEach((clicked) => {
-        if (clicked !== each) {
-          const imgClicked = clicked.querySelectorAll("img");
-          imgClicked.forEach((findOne) => {
-            if (!findOne.classList.contains("slideup_active")) {
-              findOne.classList.add("slideup_active");
-              findOne.style.height = "auto";
-              findOne.style.height = "0px";
-            }
-          });
-        }
-      });
-    });
-  }
-  each.addEventListener("click", handleClick);
+  eventList.addEventListener("click", handleEventPageModal);
 });
