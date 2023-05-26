394836b3dc9dcc7c5000cc0690cc2b3b228081cb
GeoJina <jinaseo101@gmail.com>
2023-05-26 17:18:45 +0900

    디저트&스낵 페이지 작업, 신제품 펼쳐보기 토글/슬라이더 작업

--- js/menu/new_products.js
+++ js/menu/new_products.js
@@ -26,4 +26,37 @@ $( document ).ready( function() {
       } );
     } );
   } );
+
   
+//자세히 보기 클릭시 슬라이더 펼쳐보기 
+$('.view_more').click(function(){
+  $('.new_products_slider_wrap').slideToggle("fast");
+
+  var arrowImage = $('.line_banner_right_toggle a').css('background-image');
+  if (arrowImage.includes('down-arrow.png')) {
+      $('.line_banner_right_toggle a').css('background-image', 'url(../../../img/icons/up-arrow.png)');
+  } else {
+      $('.line_banner_right_toggle a').css('background-image', 'url(../../../img/icons/down-arrow.png)');
+  }
+});
+
+
+
+
+
+
+// 신제품 슬라이더 펼쳐보기 토글 / 화살표 변경
+
+$('.line_banner_right').click(function(){
+  $('.new_products_slider_wrap').slideToggle("fast");
+
+  var arrowImage = $('.line_banner_right_toggle a').css('background-image');
+  if (arrowImage.includes('down-arrow.png')) {
+      $('.line_banner_right_toggle a').css('background-image', 'url(../../../img/icons/up-arrow.png)');
+  } else {
+      $('.line_banner_right_toggle a').css('background-image', 'url(../../../img/icons/down-arrow.png)');
+  }
+});
+
+
+
