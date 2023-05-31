8fed7aa3c3221eedaee896c15ec8eb0cee2dd62f
Hellicat <jhccop@naver.com>
2023-05-31 11:14:23 +0900

    jh_edit headbanner css

--- js/mainpage/mainquicknav.js
+++ js/mainpage/mainquicknav.js
@@ -1,4 +1,9 @@
 const quickNavBtn = document.getElementById("quicktoggle");
-
-const handleQuickNav = () => {};
-quickNavBtn.addEventListener("click", handleQuickNav);
+const quickNavBtnImg = quickNavBtn.querySelector("img");
+const quickNavCont = document.querySelector(".mainpage_quick--menu-cont");
+const handleQuickNavUp = () => {
+  quickNavBtn.classList.add("mainpage_btn");
+  quickNavBtnImg.classList.add("mainpage_btn-img");
+  quickNavCont.classList.add("mainpage_quick-cont");
+};
+quickNavBtn.addEventListener("click", handleQuickNavUp);
