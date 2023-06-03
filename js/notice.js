d71ed63f15ef7e317460b25eff802bb292abd55b
jinyong9805 <131639046+jinyoun9805@users.noreply.github.com>
2023-06-03 22:30:31 +0900

    장진용_01

--- js/notice.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-  // 페이지 넘버 클릭시
-  $(".notice .inner .page-number-box > ul > li").click(function(){
-      $(".notice .inner .page-number-box > ul > li").removeClass("active");
-      $(this).addClass("active");
-  });
-
-});
